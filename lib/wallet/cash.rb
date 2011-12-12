dd2b96e507933a25a400e5a405f76ea3dbe7ff61
bhuvan puri <puri.bhuvan@yahoo.co.in>
2011-12-12 16:26:42 -0500

    wallet

--- lib/wallet/cash.rb
+++ lib/wallet/cash.rb
@@ -4,6 +4,7 @@ module Wallet
 
     module ClassMethods
       def cash!
+
         Wallet::Configuration.controllers.each do |controller_name, actions_configs|
           controller = "#{controller_name}_controller".camelize.constantize
           actions_configs.actions.each do |action_name, ttl|
