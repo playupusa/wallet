9ee486f2db82ed8e7fa6d071557b18a142b5414c
Chien Kuo <chien.cc.kuo@gmail.com>
2012-02-07 03:53:34 -0500

    parameter cache support for get request

--- lib/wallet/cash.rb
+++ lib/wallet/cash.rb
@@ -8,7 +8,7 @@ module Wallet
         Wallet::Configuration.controllers.each do |controller_name, actions_configs|
           controller = "#{controller_name}_controller".camelize.constantize
           actions_configs.actions.each do |action_name, ttl|
-            controller.caches_action action_name, :expires_in => ttl
+            controller.caches_action action_name, :expires_in => ttl, :cache_path => Proc.new { |controller| controller.params }
           end
         end
       end
